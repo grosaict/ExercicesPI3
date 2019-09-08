8587172960904881a6a2cad805b09e8fb27914eb
Gabriel Rosa <grosa.ict@gmail.com>
2019-09-08 20:03:02 -0300

    Account classes under construction

---
+++ WorldBank/WorldBank/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldBank
+{
+    abstract class Account
+    {
+        public Persona Holder { get; set; }
+        public long AccountNumber { get; set; }
+        public int BranchOffice { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawalFee { get; set; }
+
+        public Account(Persona holder, long accountNumber, int branchOffice, double balance, double withdrawalFee)
+        {
+            Holder          = holder;
+            AccountNumber   = accountNumber;
+            BranchOffice    = branchOffice;
+            Balance         = balance;
+            WithdrawalFee   = withdrawalFee;
+        }
+
+        public virtual void Withdrawal() { }
+        public abstract double CheckBalance() { }                               // CHECK HOW TO FIX THIS ERROR WITH TEACHER
+        public abstract void Transfer(Account account, double value) { }        // CHECK HOW TO FIX THIS ERROR WITH TEACHER
+    }
+}
---
+++ WorldBank/WorldBank/CheckingAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldBank
+{
+    class CheckingAccount : Account
+    {
+        public string Type { get; }
+        public double AccountLimit { get; }
+        public double LimitFee { get; }
+
+        public CheckingAccount(Persona holder, long accountNumber, int branchOffice, double balance, double withdrawalFee)
+                    : base(holder, accountNumber, branchOffice, balance, withdrawalFee) { }
+
+        public override void Withdrawal() { }
+        public override double CheckBalance() { return 0; }
+        public override void Transfer(Account account, double value) { }
+
+    }
+}
